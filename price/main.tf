71fb9fd8c2a3724f8e4067ede06730dd2256cf49
eksuser <ragupathi121290@gmail.com>
2025-02-27 17:02:03 +0530

    teste

--- price/main.tf
+++ price/main.tf
@@ -13,11 +13,3 @@ module "vpc" {
   tags    = var.tags
 }
 
-output "vpc_id" {
-  value = module.vpc.vpc_id
-
-}
-
-output "public_ip" {
-  value = module.vpc.public_ip
-}
---
+++ price/outputs.tf
@@ -0,0 +1,8 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+
+}
+
+output "public_ip" {
+  value = module.vpc.public_ip
+}
