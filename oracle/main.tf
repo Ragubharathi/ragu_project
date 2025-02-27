05f079a7a24040ef2e5821fc6f4cdc044732b269
eksuser <ragupathi121290@gmail.com>
2025-02-27 16:03:17 +0530

    tested

--- oracle/main.tf
+++ oracle/main.tf
@@ -4,6 +4,12 @@ provider "aws" {
 
 module "vpc" {
   source = "/root/ragu_project/Modules/vpc"
-  
+  my_region  = "ap-south-1"
+  vpc_cidr   = "100.100.0.0/16"
+  AZ1 = "ap-south-1a"
+  AZ2 = "ap-south-1c"
+  pub_sub = "100.100.1.0/24"
+  pri_sub = "100.100.2.0/24"
+  tags    = "Oracle"
 }
 
