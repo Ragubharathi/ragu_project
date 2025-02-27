2081c21b158ccf5b02eaae152d12fce2a1775de6
eksuser <ragupathi121290@gmail.com>
2025-02-27 19:26:04 +0530

    import

--- import/ec2.tf
+++ import/ec2.tf
@@ -2,10 +2,83 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-
-import {
-  id = "i-036b2ef56828d4d3f"
-
-  to = aws_instance.TestServer
-
+resource "aws_instance" "TestServer" {
+  ami                                  = "ami-0d2614eafc1b0e4d2"
+  associate_public_ip_address          = true
+  availability_zone                    = "ap-south-1b"
+  disable_api_stop                     = false
+  disable_api_termination              = false
+  ebs_optimized                        = false
+  enable_primary_ipv6                  = null
+  get_password_data                    = false
+  hibernation                          = false
+  host_id                              = null
+  host_resource_group_arn              = null
+  iam_instance_profile                 = null
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = "t2.micro"
+  ipv6_address_count                   = 0
+  ipv6_addresses                       = []
+  key_name                             = "Kuberneteskeylatest"
+  monitoring                           = false
+  placement_group                      = null
+  placement_partition_number           = 0
+  private_ip                           = "172.31.2.170"
+  secondary_private_ips                = []
+  security_groups                      = ["default"]
+  source_dest_check                    = true
+  subnet_id                            = "subnet-0d8e83fabb794b0b3"
+  tags = {
+    Name = "TestServer"
+  }
+  tags_all = {
+    Name = "TestServer"
+  }
+  tenancy                     = "default"
+  user_data                   = null
+  user_data_base64            = null
+  user_data_replace_on_change = null
+  volume_tags                 = null
+  vpc_security_group_ids      = ["sg-0788a0fbd04e30cd4"]
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+  cpu_options {
+    amd_sev_snp      = null
+    core_count       = 1
+    threads_per_core = 1
+  }
+  credit_specification {
+    cpu_credits = "standard"
+  }
+  enclave_options {
+    enabled = false
+  }
+  maintenance_options {
+    auto_recovery = "default"
+  }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_protocol_ipv6          = "disabled"
+    http_put_response_hop_limit = 2
+    http_tokens                 = "required"
+    instance_metadata_tags      = "disabled"
+  }
+  private_dns_name_options {
+    enable_resource_name_dns_a_record    = true
+    enable_resource_name_dns_aaaa_record = false
+    hostname_type                        = "ip-name"
+  }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 3000
+    kms_key_id            = null
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 125
+    volume_size           = 14
+    volume_type           = "gp3"
+  }
 }
+
